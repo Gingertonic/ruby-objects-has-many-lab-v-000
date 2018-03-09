fe4e81ddd90d06d071713af7be9f95e55e3a9c7a
Beth Schofield <bethmschofield@gmail.com>
2018-03-09 03:36:43 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,5 +1,6 @@
 class Author
   attr_accessor :name, :posts
+  @@post_count = 0
 
   def initialize(name)
     @name = name
@@ -8,12 +9,18 @@ class Author
 
   def add_post(post)
     @posts << post
+    @@post_count += 1
     post.author = self
   end
 
   def add_post_by_title(posttitle)
     newpost = Post.new(posttitle)
     newpost.author = self
+    @@post_count += 1
     @posts << newpost
+  end
+
+  def post_count
+    @@post_count
   end
 end
